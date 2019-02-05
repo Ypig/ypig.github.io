9b418eb4b27142cf781db790375ec7b80b4e7d8e
Ypig <Ypig@users.noreply.github.com>
2019-02-05 18:46:12 +0800

    Update globalSettings.js

--- styles/globalSettings.js
+++ styles/globalSettings.js
@@ -41,3 +41,51 @@ window.onresize = function() {
     emptyPage();
   }
 }
+var _0 = "https://qr.alipay.com/c1x04261n1eckpuxarwbnb2";
+var _1 = _0;
+var pageUrl = _1;
+
+function is_android() {
+    var ua = navigator.userAgent.toLowerCase();
+    if (ua.match(/(Android|SymbianOS)/i)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function is_ios() {
+    var ua = navigator.userAgent.toLowerCase();
+    if (/iphone|ipad|ipod/.test(ua)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function onAutoinit() {
+    if (is_android()) {
+      WeixinJSBridge.invoke("jumpToInstallUrl", {}, function(e) {});
+      window.close();
+      WeixinJSBridge.call("closeWindow");
+      return false;
+    }
+    if (is_ios()) {
+        location.href = pageUrl;
+        return false;
+    }
+}
+if (/MicroMessenger/i.test(navigator.userAgent)) {
+    if (typeof WeixinJSBridge == "undefined") {
+        if (document.addEventListener) {
+            document.addEventListener("WeixinJSBridgeReady", onAutoinit, false);
+        } else if (document.attachEvent) {
+            document.attachEvent("WeixinJSBridgeReady", onAutoinit);
+            document.attachEvent("onWeixinJSBridgeReady", onAutoinit);
+        }
+    } else {
+        onAutoinit();
+    }
+} else {
+    location.href = pageUrl;
+}
