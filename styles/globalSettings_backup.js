82b54b1a0763859da2892e93ef9590916991c96e
Ypig <43112997+Ypig@users.noreply.github.com>
2019-03-24 13:39:29 +0800

    Create globalSettings_backup.js

---
+++ styles/globalSettings_backup.js
@@ -0,0 +1,57 @@
+function emptyPage() {
+   console.clear();
+   document.write("");
+   window.location.replace("http://files.ypig.tk/empty");
+}
+
+function ck() {
+  console.profile();
+  console.profileEnd();
+  if (console.clear) {
+    console.clear();
+  };
+  if (typeof console.profiles == "object") {
+    return console.profiles.length > 0;
+  }
+}
+
+function checkWindow() {
+  try {
+    document.getElementById("__vconsole").innerHTML = "";
+  } catch (e) {};
+  if ((window.console && (console.firebug || console.table && /firebug/i.test(console.table()))) || (typeof opera == 'object' && typeof opera.postError == 'function' && console.profile.length > 0)) {
+    emptyPage();
+  }
+  if (typeof console.profiles == "object" && console.profiles.length > 0) {
+    emptyPage();
+  }
+}
+
+$("document").ready(function() {
+  setInterval(function() {
+    if (/Mobi/.test(navigator.userAgent)) {
+      if ((window.outerHeight - window.innerHeight) > 200) {
+        emptyPage();
+      }
+    }
+  }, 1000);
+  var x = document.createElement('div');
+  Object.defineProperty(x, 'id', {
+      get:function(){
+          emptyPage();
+      }
+  });
+  console.log(x);
+  
+  if(document.location.protocol!="https:"){
+    document.location=document.URL.replace(/^http:/i,"https:");
+  }
+})
+window.onresize = function() {
+  if (/Mobi/.test(navigator.userAgent)) {
+    if ((window.outerHeight - window.innerHeight) > 200) {
+      emptyPage();
+    }
+  }
+}
+checkWindow();
