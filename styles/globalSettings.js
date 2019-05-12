5cb4ce7e35c8fa0524d4ad40fb27edb63149cfee
Ypig <43112997+Ypig@users.noreply.github.com>
2019-05-12 22:19:26 +0800

    Update globalSettings.js

--- styles/globalSettings.js
+++ styles/globalSettings.js
@@ -45,7 +45,7 @@ $("document").ready(function() {
   
   var copyrightInfo = "马雨晨(19770214)伪造官方应用，私自建立未经官方许可群聊，已严重侵犯我司版权，故禁止访问!正版黄猪官网https://ypig.tk/"
   
-      if ( /Ypiginc/.test(navigator.userAgent) !=true) {
+    if ( /Ypiginc/.test(navigator.userAgent) !=true && location.path !="/api/sendFeedBack") {
       if (document.getElementsByClassName("mdl-layout__header").length != 1 || document.getElementsByClassName("mdl-layout--fixed-header").length != 1) {
         alert(copyrightInfo);
         emptyPage();
