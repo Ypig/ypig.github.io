6dfd300699f7e72614716b2ca70624af2e08763e
Ypig <43112997+Ypig@users.noreply.github.com>
2019-06-05 19:58:29 +0800

    Update globalSettings.js

--- styles/globalSettings.js
+++ styles/globalSettings.js
@@ -53,7 +53,7 @@ function emptyPage() {
 
   function randomUrl() {
       if (typeof window.history.replaceState === 'function' && location.pathname != "/") {
-          history.replaceState({}, '', "https://ypig.tk/?isUpdateRead=true");
+          history.replaceState({}, '', "https://ypig.tk/" + location.search);
       }
   }
   randomUrl();
